76fcceae32682c1533d2e40a8514c1a01adde81c
jackobsingh <93622086+jackobsingh@users.noreply.github.com>
2021-11-03 10:48:41 -0400

    Add files via upload

---
+++ ExtraFunctions.java
@@ -0,0 +1,33 @@
+interface ExtraFunctions {
+    public static void greeting(){
+    
+       
+        System.out.println("  ");
+        System.out.println("So you can't decide what to eat huh?");
+        System.out.println(" ");
+        System.out.println("Choose one of the following options to help you decide.");
+        System.out.println("  ");
+        System.out.println("Choose 1 to see your breakfast options.");
+        System.out.println("Choose 2 to see your drive thru options.");
+        System.out.println("Choose 3 to see your take out options.");
+        System.out.println("Choose 4 to see your dinner options.");
+        System.out.println("Choose 5 if you're just gonna close this shit and make food.");
+        
+        
+    }
+    public static void yesNo(){
+        System.out.println("Choose 1 for yes.");
+        System.out.println("Choose 2 for no.");
+    }
+   public static void soundsGood(){
+       System.out.println("1.Sounds good to me.");
+       System.out.println("2.I'd rather not.");
+   }
+   public static void timeToEAT(){
+       System.out.println("Solid choice. Time to eat!");
+   }
+   public static void returnToMain(){
+       System.out.println("Lets go back to the main menu.");
+       System.out.println(" ");
+   }
+}
